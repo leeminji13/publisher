85b0f3a0edc8f4127757d894f01fc5129a1195c3
leeminji <minjis2s2s2@gmail.com>
2022-04-21 18:35:35 +0900

    스와이퍼넣고 css다시 작성

---
+++ marketing/boot4.5/js/java.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    AOS.init();
+    var swiper = new Swiper("#mySwiper", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+          // autoplay: {
+          //   delay: 3000,
+          //   disableOnInteraction: false,
+          // },
+          loop : true, 
+          spaceBetween: 30,
+          centeredSlides: true,
+          slidesPerView: 2,
+      });
+}
